bcfe67d130e8ffda436388f8f08b4020fe0e34c4
Omri Doron <omridoron3@gmail.com>
2018-01-17 18:24:24 +0200

    facebook SDK, Login

--- android/app/src/main/java/com/hotshotrussia/MainApplication.java
+++ android/app/src/main/java/com/hotshotrussia/MainApplication.java
@@ -1,7 +1,12 @@
 package com.hotshotrussia;
 
 import android.app.Application;
-
+// facebook sdk======
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.appevents.AppEventsLogger;
+// ==========
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -13,6 +18,13 @@ import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
+
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
+
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
@@ -22,7 +34,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+              new FBSDKPackage(mCallbackManager)
       );
     }
 
